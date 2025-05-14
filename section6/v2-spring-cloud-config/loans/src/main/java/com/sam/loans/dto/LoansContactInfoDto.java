dca591ff78474f428b82908a9ca6563771f25d41
samridhi_parihar <samridhi_parihar@epam.com>
2025-05-14 19:28:51 +0530

    section 2

--- section6/v2-spring-cloud-config/loans/src/main/java/com/sam/loans/dto/LoansContactInfoDto.java
+++
@@ -1,9 +0,0 @@
-package com.sam.loans.dto;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import java.util.Map;
-import java.util.List;
-
-@ConfigurationProperties(value = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails , List<String> onCallSupport) {
-}
