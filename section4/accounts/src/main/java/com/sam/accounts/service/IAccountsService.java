c57dfc3e71a09edf1c0516a281d871327a0d7730
samridhi_parihar <samridhi_parihar@epam.com>
2025-05-14 19:53:10 +0530

    section 4

---
+++ section4/accounts/src/main/java/com/sam/accounts/service/IAccountsService.java
@@ -0,0 +1,34 @@
+package com.sam.accounts.service;
+
+import com.sam.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    /**
+     *
+     * @param customerDto - CustomerDto Object
+     */
+    void createAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return Accounts Details based on a given mobile Number
+     */
+    CustomerDto fetchAccount(String mobileNumber);
+
+    /**
+     *
+     * @param customerDto - CustomerDto object
+     * @return boolean indicating if the update of Account details is successful or not
+     */
+    boolean updateAccount(CustomerDto customerDto);
+
+    /**
+     *
+     * @param mobileNumber - Input Mobile Number
+     * @return boolean indicating if the delete of Account details is successful or not
+     */
+    boolean deleteAccount(String mobileNumber);
+
+}
