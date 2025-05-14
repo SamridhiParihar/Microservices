dd2b09cb64451d6b5cf821e88bf26dcaaa904106
samridhi_parihar <samridhi_parihar@epam.com>
2025-05-14 20:02:25 +0530

    section 7

---
+++ section7/accounts/src/main/java/com/sam/accounts/dto/CustomerDto.java
@@ -0,0 +1,46 @@
+package com.sam.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.springframework.format.annotation.NumberFormat;
+
+@Data
+@Schema(
+        name = "Customer",
+        description = "Schema to hold Customer and Account information"
+)
+public class CustomerDto {
+
+    @Schema(
+            description = "Name of the customer",
+            example = "Samridhi Parihar"
+    )
+    @NotEmpty(message = "Name can't be null or empty")
+    @Size(min = 2,max = 30, message = "The length of the customer name should be between 5 and 30")
+    private String name;
+
+
+    @Schema(
+            description = "Email of the customer",
+            example = "samridhi_parihar@epam.com"
+    )
+    @NotEmpty(message = "Email can't be null or empty")
+    @Email(message = "It doesn't comply email format")
+    private String email;
+
+    @Schema(
+            description = "Mobile Number of the customer",
+            example = "9142675141"
+    )
+    @Pattern(regexp ="(^$|[0-9]{10})",message ="Mobile numbers must be 10 digits" )
+    private String mobileNumber;
+
+    @Schema(
+            description = "Account Details of the customer"
+    )
+    private AccountsDto accountsDto;
+}
